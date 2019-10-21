4543de7fc35b88a4973fbe0aa7ec341e424b1db6
Dyurekdeler <43982798+Dyurekdeler@users.noreply.github.com>
2019-10-21 18:39:40 +0300

    Firebase Actions
    firebase added to app, user creation and login has been completed.
    
    login credentials pass to another activiry is undone

--- EduSoft/app/src/main/java/com/example/edusoft/LoginActivity.java
+++ EduSoft/app/src/main/java/com/example/edusoft/LoginActivity.java
@@ -1,16 +1,24 @@
 package com.example.edusoft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,12 +27,14 @@ public class LoginActivity extends AppCompatActivity {
     private TextView signUpTxt;
     private EditText userEmail;
     private EditText userPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         loginBtn  = findViewById(R.id.loginBtn);
         forgotPwdTxt = findViewById(R.id.forgotPwdText);
         signUpTxt = findViewById(R.id.signUpTxt);
@@ -57,9 +67,24 @@ public class LoginActivity extends AppCompatActivity {
 
     private void login() {
         /* id=admin pw=admin */
-        if((!TextUtils.isEmpty(userEmail.getText()) && !TextUtils.isEmpty(userPassword.getText())) && userEmail.getText().toString().equals("admin") && userPassword.getText().toString().equals("admin") ){
-            Intent intent = new Intent(this, HomeActivity.class);
-            startActivity(intent);
+        if(!TextUtils.isEmpty(userEmail.getText()) && !TextUtils.isEmpty(userPassword.getText())){
+            mAuth.signInWithEmailAndPassword(userEmail.getText().toString(), userPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("loginTag", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+
+                            }// ...
+                        }
+                    });
+
         }else{
             Toast.makeText(getApplicationContext(),"Please fill all the required fields",Toast.LENGTH_SHORT).show();
         }
--- EduSoft/app/src/main/java/com/example/edusoft/SignUpActivity.java
+++ EduSoft/app/src/main/java/com/example/edusoft/SignUpActivity.java
@@ -1,28 +1,49 @@
 package com.example.edusoft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.INotificationSideChannel;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private CheckBox showPwdCheckBox;
     private EditText userPwd;
+    private EditText userEmail;
+    private Button signUpButton;
+    private ProgressBar mProgressBar;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mAuth = FirebaseAuth.getInstance();
         showPwdCheckBox = findViewById(R.id.showPwdCheckBox);
         userPwd = findViewById(R.id.signUpUserPwd);
+        userEmail = findViewById(R.id.signUpUserEmail);
+        signUpButton = findViewById(R.id.signUpButton);
+        mProgressBar = findViewById(R.id.signUpProgress);
 
         showPwdCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -31,6 +52,37 @@ public class SignUpActivity extends AppCompatActivity {
                     userPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
                 }else{
                     userPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());;
+                }
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProgressBar.setVisibility(View.VISIBLE);
+                if(!TextUtils.isEmpty(userEmail.getText().toString()) && !TextUtils.isEmpty(userPwd.getText().toString())) {
+                    mAuth.createUserWithEmailAndPassword(userEmail.getText().toString(), userPwd.getText().toString())
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        mProgressBar.setVisibility(View.GONE);
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("userTag", "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_SHORT).show();
+                                    }
+                                    // ...
+                                }
+                            });
+                }else{
+                    Toast.makeText(getApplicationContext(),"Empty fields are not allowed",Toast.LENGTH_SHORT).show();
                 }
             }
         });
